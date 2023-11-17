dd7e2b3663c1cfea8042b09a640a5eedc0fc6422
Nuno Fachada <faken@fakenmc.com>
2023-11-17 21:39:31 +0000

    Document text_stats + if'main it

--- txtstats/core.py
+++ txtstats/core.py
@@ -1,11 +1,26 @@
+"""
+Functions for generating text statistics.
+"""
+
 from typing import Any
 
-def text_stats(txt: str) -> dict[str, Any]:
 
+def text_stats(txt: str) -> dict[str, Any]:
+    """Generate statistics for the specified text.
+
+    Args:
+      s:
+        The text from which to generate statistics.
+
+    Returns:
+      A dictionary containing the generated text statistics.
+    """
     wc = len(txt.split())
     lc = len(txt.split("\n"))
     cc = len(txt)
 
-    return { "word_count": wc, "line_count": lc, "char_count": cc }
+    return {"word_count": wc, "line_count": lc, "char_count": cc}
 
-print(text_stats("Hello world\nBye world"))
+
+if __name__ == "__main__":
+    print(text_stats("Hello world\nBye world"))
