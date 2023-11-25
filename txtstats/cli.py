52310f9b33cbb2a72f820c5888af5f1e3a216afc
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-11-25 17:36:02 +0000

    Add tabulate dependency and print nice output

--- txtstats/cli.py
+++ txtstats/cli.py
@@ -4,6 +4,7 @@ Functions for to be invoked as command line scripts.
 
 import sys
 from inspect import getdoc
+from tabulate import tabulate
 
 
 def _main():
@@ -13,11 +14,25 @@ def _main():
         f = sys.argv[1]
         with open(f, "r") as text_file:
             s_stats = text_stats(text_file.read())
-        for stat_name in s_stats:
-            print(f"\t{stat_name} : {s_stats[stat_name]}")
+        print(
+            tabulate(
+                s_stats.items(),
+                headers=["Statistic", "Value"],
+                tablefmt="rounded_outline",
+            )
+        )
+
     else:
         from .stats_finder import _find_stats
 
         stats_found = _find_stats()
-        for stat_name in stats_found:
-            print(stat_name, "-", getdoc(stats_found[stat_name]).split("\n")[0])
+
+        stats_help = {k: getdoc(v).split("\n")[0] for k, v in stats_found.items()}
+
+        print(
+            tabulate(
+                stats_help.items(),
+                headers=["Statistic", "Description"],
+                tablefmt="rounded_outline",
+            )
+        )
