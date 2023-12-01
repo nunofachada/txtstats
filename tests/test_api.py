fbbff3dea03cc11d5e9f9f554fe91abd08c8da58
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-12-01 23:16:39 +0000

    Add API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,29 @@
 """Tests for the API module."""
+
+import pytest
+
+from txtstats import text_stats
+
+
+def test_text_stats():
+    """Test text_stats() function for expected output."""
+    d = text_stats("Hello world!")
+    assert isinstance(d, dict)
+
+    for key in d:
+        assert isinstance(key, str)
+
+
+def test_text_stats_errors():
+    """Test text_stats() function for expected errors."""
+    with pytest.raises(TypeError):
+        text_stats(1)
+
+    with pytest.raises(TypeError):
+        text_stats(1.34)
+
+    with pytest.raises(TypeError):
+        text_stats(["hello", "world"])
+
+    with pytest.raises(TypeError):
+        text_stats(("hello", "world", 1, 2, 3))
