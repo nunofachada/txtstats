c10ced5aea5dde1cc3c12b377946473e3bd36070
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-12-02 19:41:31 +0000

    Rename test_*_count() to test_*_count_result()

--- tests/test_stats_functions.py
+++ tests/test_stats_functions.py
@@ -3,16 +3,16 @@
 from txtstats.stats_functions import _char_count, _line_count, _word_count
 
 
-def test_char_count():
+def test_char_count_result():
     """Test the _char_count function."""
     assert _char_count("Hello world!") == 12
 
 
-def test_line_count():
+def test_line_count_result():
     """Test the _line_count function."""
     assert _line_count("Hello world!") == 1
 
 
-def test_word_count():
+def test_word_count_result():
     """Test the _word_count function."""
     assert _word_count("Hello world!") == 2
