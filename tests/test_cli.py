9b17170f2a1ccb207b77479215372ccb0ec69560
Nuno Fachada <faken@fakenmc.com>
2023-12-04 18:04:33 +0000

    Test output of cli function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,18 @@
 """Tests for the CLI module."""
+
+import re
+import sys
+
+from txtstats.cli import _main
+
+
+def test_main_output(tmp_path, capsys, monkeypatch, string_to_test):
+    """Test the print function prints to the console."""
+    file = tmp_path / "file_to_test.txt"
+    file.write_text(string_to_test)
+    monkeypatch.setattr(sys, "argv", [sys.argv[0], str(file)])
+    _main()
+    output = capsys.readouterr().out.rstrip()
+    assert re.search("char_count(.*)12", output)
+    assert re.search("line_count(.*)1", output)
+    assert re.search("word_count(.*)2", output)
