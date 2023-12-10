85ded867e540bb7a58cb89aab9c912ed1d79c1e4
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-12-10 18:50:11 +0000

    Improve package docs

--- txtstats/__init__.py
+++ txtstats/__init__.py
@@ -1,4 +1,13 @@
-"""**txtstats** calculates text statistics."""
+"""
+![txtstats status](https://github.com/nunofachada/txtstats/actions/workflows/test.yml/badge.svg)
+[![codecov](https://codecov.io/gh/nunofachada/txtstats/graph/badge.svg?token=skfov5ogSx)](https://codecov.io/gh/nunofachada/txtstats)
+
+**txtstats** is an educational research software example for calculating text statistics.
+
+## License
+
+[MIT](LICENSE)
+"""
 
 __all__ = ["text_stats"]
 
